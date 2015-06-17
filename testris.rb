9a84ceb51f31fba08e2cf1973b1e4aa2a9d8912a
kmooney <kmooney@gmail.com>
2015-06-17 18:35:54 -0500

    Fixed collision detection, improved grace period - needs a bit more logic yet

--- testris.rb
+++ testris.rb
@@ -1,7 +1,7 @@
 require 'gosu'
 CONFIGS = [[2,7,1,0,1,1,1,0],[2,2,1,1,1,1],[2,3,1,0,1,0,1,1],[2,4,0,1,0,1,1,1],[2,5,1,0,1,1,0,1],[2,6,0,1,1,1,1,0],[1,8,1,1,1,1]]
-COLORS = [ Gosu::Color.argb(0xff_202020), Gosu::Color.argb(0xff_666666), Gosu::Color.argb(0xff_ffff66), Gosu::Color.argb(0xff_66ffff), Gosu::Color.argb(0xff_ff6666), Gosu::Color.argb(0xff_0033cc), Gosu::Color.argb(0xff_ff6666), Gosu::Color.argb(0xff_999966), Gosu::Color.argb(0xff_ff66ff),]
-BOARD_HEIGHT, BOARD_WIDTH, UNIT, PADDING = 20, 10, 50, 2  # default: 20, 10, 30
+COLORS = [Gosu::Color.argb(0xff_202020), Gosu::Color.argb(0xff_666666), Gosu::Color.argb(0xff_ffff66), Gosu::Color.argb(0xff_66ffff), Gosu::Color.argb(0xff_ff6666), Gosu::Color.argb(0xff_0033cc), Gosu::Color.argb(0xff_ff6666), Gosu::Color.argb(0xff_999966), Gosu::Color.argb(0xff_ff66ff)]
+BOARD_HEIGHT, BOARD_WIDTH, UNIT, PADDING = 20, 10, 40, 2  # default: 20, 10, 30
 class Piece
     attr_reader :r, :c, :width, :config, :color
     def initialize(config=nil)
@@ -11,22 +11,30 @@ class Piece
         @start_width = @width
         @timer = Gosu::milliseconds()
     end
-    def rotate_cw(r=@config.length/@width)
+    def check(grid, r=-1)
+        r += @r
+        @config.each_with_index{ |el, i|
+            c, r = i % @width + @c, i % @width == 0 ? r+1 : r
+            return false if c < 0 or c>=BOARD_WIDTH or (el != 0 and grid[r][c] == 1)}
+        return true
+    end
+    def rotate_cw(grid, r=@config.length/@width)
         @config = @config.each_with_index.map{|__,i| @config[@width * (r - i % r - 1) + i/r]}
         @width = @width != @start_width ? @start_width : @config.length / @width
         @c = BOARD_WIDTH - @width if @c + @width > BOARD_WIDTH
+        return check(grid)
     end
-    def rotate_ccw
-        (1..3).each do rotate_cw end
+    def rotate_ccw(grid)
+        (1..3).each do rotate_cw grid end
+        return check(grid)
     end
     def left(grid)
-        @c -= 1 if (@c > 0) and grid[@r][@c-1] == 0
+        @c -= 1
+        return check(grid)
     end
     def right(grid)
-        @c += 1 if (@c+@width < BOARD_WIDTH) and grid[@r][@c+1] == 0
-    end
-    def place(grid)
-        drop 0 while not placed? grid
+        @c += 1
+        return check(grid)
     end
     def draw(ox=0, oy=0, r=-1)
         @config.each_with_index{|bit, i|
@@ -35,9 +43,7 @@ class Piece
            Gosu::draw_rect(x,y,UNIT-PADDING,UNIT-PADDING,COLORS[@color],3) if bit != 0 }
     end
     def drop(cooldown=500)
-        if Gosu::milliseconds() - @timer > cooldown
-            @r, @timer = @r + 1, Gosu::milliseconds()
-        end
+        (@r, @timer = @r + 1, Gosu::milliseconds()) if Gosu::milliseconds() - @timer > cooldown
     end
     def placed?(grid, r=-1)
        @config.each_with_index{|bit, i| 
@@ -49,28 +55,31 @@ end
 class Testris < Gosu::Window
     def initialize
         super BOARD_WIDTH*3*UNIT/2, BOARD_HEIGHT*UNIT
-        @cooldown, @last_hit = 50, 0
+        @cooldown, @last_hit, @locked = 50, 0, false
         @next, @current, @grid = Piece.new, Piece.new, Array.new(BOARD_HEIGHT) { Array.new(BOARD_WIDTH) { 0 } }
         @lines, @font = 0, Gosu::Font.new(self, "media/minecraftia.ttf", UNIT)
     end
     def button_up(id)
-        @current.place(@grid) if id == Gosu::KbDown 
-        @current.rotate_ccw if id == Gosu::KbQ 
-        @current.rotate_cw if id == Gosu::KbW
+        check = Marshal::load(Marshal::dump(@current))
+        (@current.drop 0 while not @current.placed? @grid) if id == Gosu::KbDown
+        @current.rotate_ccw @grid if id == Gosu::KbQ and check.rotate_ccw @grid
+        @current.rotate_cw @grid if id == Gosu::KbW and check.rotate_cw @grid
+        @locked = true if @current.placed? @grid
     end
     def update(r=-1)
         if Gosu::milliseconds() - @last_hit > @cooldown
-            @current.left @grid if Gosu::button_down? Gosu::KbLeft
-            @current.right @grid if Gosu::button_down? Gosu::KbRight
+            check = Marshal::load(Marshal::dump(@current))
+            @current.left @grid if Gosu::button_down? Gosu::KbLeft and check.left @grid
+            @current.right @grid if Gosu::button_down? Gosu::KbRight and check.right @grid
             close if Gosu::button_down? Gosu::KbEscape
             @last_hit = Gosu::milliseconds()
         end
         if @current != nil 
-            if @current.placed? @grid
+            if @current.placed? @grid and @locked
                 @current.config.each_with_index{|bit, i|
                     c, r = (i % @current.width), (i % @current.width == 0 ? r+1 : r)
                     @grid[@current.r + r][@current.c + c] = 1 if bit != 0 }
-                @current, @next = @next, Piece.new
+                @current, @next, @locked = @next, Piece.new, false
             end 
             @grid.delete(Array.new(BOARD_WIDTH){1})
             @lines += BOARD_HEIGHT-@grid.length
@@ -84,7 +93,6 @@ class Testris < Gosu::Window
         @current.draw
         @font.draw "Lines: #{@lines}", BOARD_WIDTH*UNIT+UNIT/10, 0, 99
         @font.draw "Next", BOARD_WIDTH*UNIT+UNIT/10, UNIT*2, 99
-        @font.draw "Use arrow keys to move piece, 'Q' and 'W' to rotate.", UNIT*2, UNIT*10, 30 if Gosu::milliseconds() < 5000
         @next.draw 6.5,2.2
     end
 end
